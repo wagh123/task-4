6acb540189d024d4aff91031b66cdfa4e0829d21
yogita wagh <72869514+wagh123@users.noreply.github.com>
2022-12-10 04:30:07 +0530

    Add files via upload

---
+++ styles.css
@@ -0,0 +1,48 @@
+.container{
+    text-align: center;
+    margin-top:23px
+}
+body{
+    background-color: gray;
+}
+.h2{
+    margin-bottom: 10px;
+    font-size: 50px;
+    text-align: center;
+}
+table{
+    margin: auto;
+}
+
+input{
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border-radius: 21px;
+    border: 5px solid #777777;
+    font-size:34px;
+    height: 65px;
+    width: 456px;
+}
+
+button{
+    border-radius: 10px;
+    font-size: 40px;
+    background: #a5a5a5;
+    width: 102px;
+    height: 70px;
+    margin: 6px;
+}
+
+.calculator{ 
+    border: 4px solid #a9a9a9;
+    background-color: #000000;
+    padding: 23px;
+    border-radius: 20px;
+    display: inline-block;
+    
+}
+
+h1{
+    font-size: 28px;
+    font-family: 'Courier New', Courier, monospace;
+}
